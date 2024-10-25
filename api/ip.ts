6ebc7845ae515e92b964a9bab4224b01fb3d5faf
trudbot <SYP2185759039@163.com>
2024-10-25 17:13:45 +0800

    fix: stringify位置数据

--- api/ip.ts
+++ api/ip.ts
@@ -1,4 +1,4 @@
 import {ipAddress, geolocation} from "@vercel/functions"
 export function GET(request: Request) {
-  return new Response(`Your IP address is ${ipAddress(request)}, and your geolocation is ${geolocation(request)}`);
+  return new Response(`Your IP address is ${ipAddress(request)}, and your geolocation is ${JSON.stringify(geolocation(request))}`);
 }
